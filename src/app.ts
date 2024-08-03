ad91906a65f5338ee4742c0717f974293bcbaab2
DanielSiilva <daniellsousa0614@gmail.com>
2024-08-03 20:00:11 -0300

    refactor: init server

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,16 @@
-console.log("Hello word!");
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+const PORT = 5001;
+
+const server = async () => {
+  try {
+    app.listen(PORT, () => {
+      console.log(`Servidor rodando na porta ${PORT}`);
+    });
+  } catch (error) {}
+};
+
+server();
