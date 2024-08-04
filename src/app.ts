b22fc7fc287000f2921efefa8c0f0cc764b496e3
DanielSiilva <daniellsousa0614@gmail.com>
2024-08-04 11:58:55 -0300

    feat: config env variables

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,19 @@
 import express from "express";
 import routes from "./routes/authRoutes";
+import dotenv from "dotenv";
+
+dotenv.config();
 const app = express();
 app.use(express.json());
-
-const PORT = 5001;
+const PORT = process.env.PORT;
 
 app.use(routes);
 
 const server = async () => {
+  const keyscret = process.env.JWT_PRIVATE_KEY_PATH;
+
+  console.log("key", keyscret);
+
   try {
     app.listen(PORT, () => {
       console.log(`Servidor rodando em: http://localhost:${PORT}`);
